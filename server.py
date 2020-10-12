d9a46768c0012bdde8a1cf56c8ef2dc59196305b
Angelos Evripiotis <aevri@users.noreply.github.com>
2020-10-12 16:29:32 +0100

    server.py: return a random word

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
 import os
 import http.server
+import random
 import socketserver
+import urllib.request
 
 from http import HTTPStatus
+
+WORDS_URL = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+
+with urllib.request.urlopen(WORDS_URL) as response:
+   WORDS = text.splitlines(response.read())
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        msg = 'Your random word is: ' + random.choice(WORDS)
         self.wfile.write(msg.encode())
 
 
